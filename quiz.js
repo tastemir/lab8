e7cf5da5b2c5e6ae01774c4a1bb7cca4ca5d6191
tastemir <44054302+tastemir@users.noreply.github.com>
2023-04-04 11:02:37 +0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    // define global arrays for correct answers, questions, and choice options
+    var correct = ["2009", "Astana"];
+    var questions = ["Which year was Nazarbayev University founded?", "What is the capital of Kazakhstan?"];
+    var choice_options = [["2009","2010","2011","2012"],["Astana","Almaty","Shymkent","Karaganda"]];
+
+    // generate quiz questions
+    for (var i = 0; i < questions.length; i++) {
+        // create a new div for each question
+        var questionDiv = $("<div class='question'></div>");
+        // add the question text
+        questionDiv.append("<p class='d'>Q" + (i+1) + ". " + questions[i] + "</p>");
+        // add the answer options
+        for (var j = 0; j < choice_options[i].length; j++) {
+            questionDiv.append("<label><input type='radio' name='q" + (i+1) + "' value='" + choice_options[i][j] + "'>" + choice_options[i][j] + "</label>");
+        }
+        // append the question div to the quiz box
+        $("#quiz-box").append(questionDiv);
+    }
+
+    // initialize count of correct answers
+    var countCorrect = 0;
+
+    // attach click event handler to button
+    $("button").click(function(event){
+        // check answers
+        for (var i = 0; i < correct.length; i++) {
+            var userAnswer = $("input[name=q" + (i+1) + "]:checked").val();
+            if (userAnswer == correct[i]) {
+                countCorrect++;
+            }
+        }
+        // display overall score
+        $("#quiz-box").append("<h2>Result of the quiz is: " + countCorrect + "</h2>");
+    });
+});
